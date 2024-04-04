6e98481de3664b1661fdabd657c369af0d8fa578
AguSSantos Prieto Atienza <131393036+agusSantos23@users.noreply.github.com>
2024-04-04 10:12:28 +0200

    2

--- submit.php
+++ submit.php
@@ -1,26 +1,32 @@
 <?php
+// Conexión a la base de datos
 $servername = "localhost";
 $username = "root";
 $password = "";
 $database = "formulario";
-// Create connection
-$conn = new mysqli($servername, $username, $password,$database);  
-// Check connection
-if ($conn->connect_error) {  die("Connection failed: " . $conn->connect_error);}
-mysqli_select_db($conn,$database);
 
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $database);
 
-$nombre = $_POST['Nombre'];
-$email = $_POST['email'];
-$mensaje = $_Post['mensaje'];
-
-$sql = "INSETT INTO formulario_contacto (nombre,email,mensaje) VALUES ("$nombre","$email","$mensaje")";
-
-if($conn->query($sql) === TRUE){
-    echo "ole";
-}else{
-    echo "Error:" . $sql . "<br>" . $conn->error;
+// Verificar conexión
+if ($conn->connect_error) {
+    die("La conexión falló: " . $conn->connect_error);
 }
 
+// Obtener datos del formulario
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$mensaje = $_POST['mensaje'];
+
+// Insertar datos en la base de datos
+$sql = "INSERT INTO formulario_contacto (nombre, email, mensaje) VALUES ('$nombre', '$email', '$mensaje')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "¡Mensaje enviado correctamente!";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+// Cerrar conexión
 $conn->close();
 ?>
